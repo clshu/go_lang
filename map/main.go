22338ac04b34f8a6ccf499c68f8989c1fc8b1623
clshu <ccss0630@yahoo.com>
2019-04-24 16:27:34 -0700

    3 ways of initilazing map

---
+++ map/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":  "#ff0000",
+		"lime": "#00ff00",
+	}
+
+	var colors1 map[string]string
+
+	colors2 := make(map[string]string)
+
+	fmt.Println(colors)
+	fmt.Println(colors1)
+	fmt.Println(colors2)
+}
